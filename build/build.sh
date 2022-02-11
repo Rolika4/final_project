fa86b5f8c586e882bfbb3425522877c9775db2e1
Mykola_Serdiuk <kolyaaxe@gmail.com>
2022-02-12 01:45:30 +0200

    Add:

---
+++ build/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /home/ubuntu/realworld-backend-spring-ddd/
+chmod +x mvnw
+make
+cp /home/ubuntu/realworld-backend-spring-ddd/target/backend-0.0.1-SNAPSHOT.jar /home/ubuntu/git/JavaApp/app.jar
+cd /home/ubuntu/git/JavaApp/
+docker build -t rolliku/backend .
+cd /home/ubuntu/Client
+npm i
+npm run build
+cp -R /home/ubuntu/Client/build /home/ubuntu/git/React/
+cd /home/ubuntu/git/React
+sudo docker build -t rolliku/frontend .
