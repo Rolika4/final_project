d14416992b6dbf1d323ed9a461fb554634818963
Nick Serdiuk <52344283+Rolika4@users.noreply.github.com>
2022-02-12 23:42:18 +0300

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/ubuntu/realworld-backend-spring-ddd/
-chmod +x mvnw
-make
-cp /home/ubuntu/realworld-backend-spring-ddd/target/backend-0.0.1-SNAPSHOT.jar /home/ubuntu/git/JavaApp/app.jar
+cd /home/ubuntu/spring-boot-realworld-example-app
+chmod +x gradlew
+./gradlew build
+cp /home/ubuntu/spring-boot-realworld-example-app/build/libs/spring-boot-realworld-example-app-0.0.1-SNAPSHOT.jar /home/ubuntu/git/JavaApp/app.jar
 cd /home/ubuntu/git/JavaApp/
 docker build -t rolliku/backend .
 cd /home/ubuntu/Client
